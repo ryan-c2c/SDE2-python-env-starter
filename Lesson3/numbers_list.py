c0d8fffaeb44623766acbab6ddc062d6885c4ab9
Zachary Smith <156365790+zsmithc2c@users.noreply.github.com>
2025-06-03 01:04:38 +0000

    lesson 3

---
+++ Lesson3/numbers_list.py
@@ -0,0 +1,6 @@
+total = 0
+numbers = [1, 2, 3]
+
+for number in numbers:
+    total += number
+print(total)
---
+++ Lesson3/presidents_list.py
@@ -0,0 +1,9 @@
+# How can we use loops to reduce the necessary code that does the following
+presidents = ['Washington','Adams','Jefferson']
+
+print(f"{presidents[0]} was president #1")
+print(f"{presidents[1]} was president #2")
+print(f"{presidents[2]} was president #3")
+
+# How do you get the index (the number between square brackets [])?
+# What happens if we add a president to the list? What happens if we remove a president from the list?
